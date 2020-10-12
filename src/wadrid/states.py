21c427e498bb92c525faf2b6b30f95ed1fffd9d5
David Lilue <2525462+dvdalilue@users.noreply.github.com>
2020-10-12 17:02:32 +0200

    States definition. Initial, Prelude, Frame (w/ internal) and Final. Only 'en' i18n

--- src/wadrid/states.py
+++ src/wadrid/states.py
@@ -2,6 +2,9 @@
 from bernard import (
     layers as lyr,
 )
+from bernard.platforms.telegram import (
+    layers as tgr,
+)
 from bernard.analytics import (
     page_view,
 )
@@ -9,9 +12,17 @@ from bernard.engine import (
     BaseState,
 )
 from bernard.i18n import (
+    intents as its,
+)
+from bernard.i18n import (
     translate as t,
 )
 
+from .store import (
+    cs,
+    LoopContext,
+    FrameContext,
+)
 
 class WadridState(BaseState):
     """
@@ -38,22 +49,128 @@ class WadridState(BaseState):
         transitions.
         """
 
-        self.send(lyr.Text(t.CONFUSED))
+        self.send(
+            lyr.Markdown(t.HELP),
+            lyr.Markdown(t.START_CMD),
+            lyr.Markdown(t.SEARCH_CMD),
+        )
 
     async def handle(self) -> None:
         raise NotImplementedError
 
+    def reply_keyboard():
+        """
+        Common reply keyboard among the states. Two options "yes" or "no".
+        """
 
-class Hello(WadridState):
+        return tgr.ReplyKeyboard(
+            keyboard=[
+                [
+                    tgr.KeyboardButton(
+                        text='No',
+                        choice='no',
+                        intent=its.NO,
+                    ),
+                    tgr.KeyboardButton(
+                        text='Yes',
+                        choice='yes',
+                        intent=its.YES,
+                    )
+                ],
+            ],
+            resize_keyboard=True,
+        )
+
+class S000xInitial(WadridState):
     """
-    Example "Hello" state, to show you how it's done. You can remove it.
-
-    Please note the @page_view decorator that allows to track the viewing of
-    this page using the analytics provider set in the configuration. If there
-    is no analytics provider, nothing special will happen and the handler
-    will be called as usual.
     """
 
-    @page_view('/bot/hello')
-    async def handle(self):
-        self.send(lyr.Text(t.HELLO))
+    @page_view('/bot/initial')
+    @cs.inject()
+    async def handle(self, context):
+        name = await self.request.user.get_friendly_name()
+        text = lyr.Text(t('WELCOME', name=name))
+        loop_context = LoopContext(context)
+
+        if loop_context.in_loop():
+            text = lyr.Text(t.LOOP)
+        else:
+            await FrameContext(context).init_context()
+
+        loop_context.exit_loop()
+        context['started'] = False
+
+        self.send(
+            text,
+            WadridState.reply_keyboard()
+        )
+
+class S001xPrelude(WadridState):
+    """
+    """
+
+    @page_view('/bot/begin')
+    @cs.inject()
+    async def handle(self, context):
+        text = lyr.Text(t.CONTINUE)
+
+        if not context.get('started'):
+            text = lyr.Text(t.BEGIN)
+            FrameContext(context).configure_context()
+            context['started'] = True
+
+        self.send(text, WadridState.reply_keyboard())
+
+class S002xFrame(WadridState):
+    """
+    """
+
+    @page_view('/bot/search')
+    @cs.inject()
+    async def handle(self, context):
+        url = FrameContext(context).get_image_url()
+
+        cancel_kb = tgr.InlineKeyboard([
+            [tgr.InlineKeyboardCallbackButton(
+                text='Stop search',
+                payload={ 'action': 'cancel' },
+            )],
+        ])
+
+        self.send(lyr.Text(url), WadridState.reply_keyboard())
+        self.send(lyr.Text(t.FRAME), cancel_kb)
+
+class S003xFrameInternal(WadridState):
+    """
+    """
+
+    @page_view('/bot/search/aux')
+    @cs.inject()
+    async def handle(self, context):
+        frame_ctx = FrameContext(context)
+        current_frame = frame_ctx.get_current_frame()
+
+        if self.trigger.launched:
+            frame_ctx.set_max_frame(current_frame)
+        else:
+            frame_ctx.set_min_frame(current_frame)
+
+        frame_ctx.bisect()
+
+
+class S004xFinal(WadridState):
+    """
+    """
+
+    @page_view('/bot/final')
+    @cs.inject()
+    async def handle(self, context):
+        frame_ctx = FrameContext(context)
+        current_frame = frame_ctx.get_current_frame()
+
+        frame_ctx.configure_context()
+
+        self.send(
+            lyr.Text(t('FINAL', frame=current_frame)),
+            WadridState.reply_keyboard()
+        )
